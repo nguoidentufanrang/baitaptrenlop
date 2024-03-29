32ab68fe5907f1b7fb01bfb3a19415f823a9f07d
vitin <vitin@ADMIN>
2024-03-29 23:47:04 +0700

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using baitaptrenlop.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace baitaptrenlop.Models
+{
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        // One-to-many relationship with books
+        public List<Book>? Books { get; set; }
+    }
+}
